10b2cd777685cd3e7aa23e69922e8d28ce34432e
beibei <954429460@qq.com>
2019-04-28 17:05:09 +0800

    人员管理模块

--- index.js
+++ index.js
@@ -7,62 +7,72 @@ app.use(cors({
     methods:['GET'],  //指定接收的请求类型
     alloweHeaders:['Content-Type','Authorization']  //指定header
 }))
+let pools =  mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '831015',
+  database: 'mysql',
+  port: '3306'
+})
 // 登陆
 app.get('/login', (req, res) => {
-    // let sql
-    // if(req.query.select == 1) {
-    //     sql = `select * from mysql.student where id=${req.query.id} AND password=${req.query.password}`
-    // } else if(req.query.select == 2) {
-    //     sql = `select * from mysql.manager where id=${req.query.id} AND password=${req.query.password}`
-    // }
-    // // 获取查询语句
-    // mysql.createConnection({
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: '831015',
-    //     database: 'mysql',
-    //     port: '3306'
-    // }).then(function(conn){
-    //     var result = conn.query(sql);
-    //     conn.end();
-    //     return result;
-    // }, function() {
-    //     conn.end();
-    //     res.end('无权限')
-    // }).then(function(rows){
-    //     // Logs out a list of hobbits
-    //     console.log(rows);
-    //     console.log({data: rows})
-    //     res.send(rows)
-    // });
+  let sql
+  sql = `select * from mysql.user where username=${req.query.username} AND password=${req.query.password}`
+  pools.query(sql).then(function(rows){
+    console.log({code: 200, data: rows})
+    res.send({code: 200, data: rows})
+  }).catch(e => {
+    console.log(e)
+  });
+})
+// 添加管理员
+app.get('/createUser', (req, res) => {
+  let target = JSON.parse(req.query.form)
+  let createSql = `INSERT INTO mysql.user (uid, name, username, permission, password) VALUES(uuid(), ${target.name}, ${target.username}, ${target.permission}, ${target.newpassword})`
+  let searchSql = `select * from mysql.user where username=${target.username}`
+  pools.query(searchSql).then((rows) => {
+    if (rows.length > 0) {
+      res.send({
+        code: 200, data: rows
+      })
+    } else {
+      pools.query(createSql).then((rows) => {
+        res.send({code: 200, data: rows})
+      }).catch(e => {
+        console.log(e)
+      })
+    }
+  })
+})
+// 获取非一级管理员信息
+app.get('/getManager', (req, res) => {
+  let sql = 'select * from mysql.user where permission = 0'
+  pools.query(sql).then((rows) => {
+    res.send({
+      code: 200,
+      data: rows
+    })
+  })
+})
+// 更改管理员信息
+app.get('/upDataManagerInfo', (req, res) => {
+  // JSON.stringify
+  let username = JSON.parse(req.query.username)
+  // let uid = JSON.parse(req.query.uid)
+  console.log(username)
+  let sql = `UPDATE mysql.user SET permission = 1 where username = ${username}`
+  pools.query(sql).then((rows) => {
+    console.log(rows, 999)
+    res.send({
+      code: 200,
+      data: rows
+    })
+  }).catch( e => {
+    console.log(e)
+  })
 })
 // 获取教室表
 app.get('/classroom', (req, res) => {
-    // console.log('classroom enter')
-    // let sql = `select * from mysql.classroom`
-    // // 获取查询语句
-    // mysql.createConnection({
-    //     host: 'localhost',
-    //     user: 'root',
-    //     password: '831015',
-    //     database: 'mysql',
-    //     port: '3306'
-    // }).then(function(conn){
-    //     var result = conn.query(sql);
-    //     conn.end();
-    //     return result;
-    // }, function() {
-    //     conn.end();
-    //     res.end('无权限')
-    // }).then(function(rows){
-    //     console.log(rows);
-    //     let classroom = []
-    //     rows.map( e => {
-    //         classroom.push({'label': e.no.toString()})
-    //     })
-    //     console.log({table: classroom})
-    //     res.send({table: classroom})
-    // });
 })
 // 获取教室详情
 app.get('/classroomDetail', (req, res) => {
